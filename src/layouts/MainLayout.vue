6071b7ae436b5a0121e61e818f2382d94f01302d
victorfernandes96 <victorfernandes1996@hotmail.com>
2024-01-23 18:55:59 -0300

    upload my first project with quasar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="flex justify-between items-center q-pa-md">
+      <q-icon name="keyboard_arrow_left" size="25px"> </q-icon>
+      <q-toolbar-title class="text-center"> Master Learning </q-toolbar-title>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="leftDrawerOpen = !leftDrawerOpen"
+      />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Master Learning First Project
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Inicio",
+    caption: "Inicio da Aplicação",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Cursos",
+    caption: "ir para site de Cursos",
+    icon: "code",
+    link: "https://jacode.com.br",
+  },
+];
+
+export default {
+  name: "MainLayout",
+  components: {
+    EssentialLink,
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+    };
+  },
+};
+</script>
+
+<style lang="stylus">
+.q-header {
+  background-color: #f95179;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-page class="container flex flex-center justify-around">
+    <q-img
+      src="https://play-lh.googleusercontent.com/q2RJGwjMz5VUFHhwhyYFs2toYEzX9t6JVn-94Hi7hXxN6Hw3RQgfmbzVtHA9ww-oZb0r"
+      spinner-color="white"
+      style="height: auto; max-width: 230px"
+    />
+
+    <h4 class="title q-ma-sm text-center">{{ phrases[index] }}</h4>
+
+    <q-btn
+      @click="randonPosition"
+      class="button"
+      rounded
+      label="Gerar Frases"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PhraseOfDay",
+  data() {
+    return {
+      phrases: [
+        "Se você aprendeu a ter medo, então poderá aprender a ter coragem.",
+        "A timidez faz com que um homem negue ao mundo a sua existência.",
+        "A dor faz parte, o sofrimento é uma opção e o sucesso a consequência.",
+        "O pior não, é aquele que você dá a si mesmo",
+        "Os diamantes são valiosos não por serem brilhantes, mas por serem raros",
+      ],
+
+      index: 0,
+    };
+  },
+
+  methods: {
+    randonPosition() {
+      let index = parseInt(Math.random() * 5 + 0);
+      console.log(index);
+      this.index = index;
+    },
+  },
+
+  beforeMount() {
+    this.randonPosition();
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.container {
+   background-color: #f95179;
+  }
+
+  .title {
+    color: #fff
+  };
+
+  .button{
+
+    color: #fff;
+    height: 60px;
+    background-color: #262525;
+  }
+</style>
